f996feafe6c81c9f59345ae6c20c7a267bb513ea
feng65 <1776338251@qq.com>
2020-03-06 17:27:54 +0800

    完成大部分主页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,45 +1,76 @@
 <template>
   <div class="box">
     <div class="left">
+      <!-- photo -->
       <div class="left_top">
-        <div class="card">
+        <div class="card photo_card">
           <p class="title">Photo</p>
+          <div class="photo_box">
+            <img class="photo_img" src="@/assets/img/myfont.png" alt="">
+          </div>
         </div>
       </div>
+      <!-- mail -->
       <div class="left_bottom">
-        <div class="card">
-          <p class="title" @click="att()">Photo</p>
+        <div class="card mail_box">
+          <p class="title" @click="att()">Mail</p>
+          <div class="photo_box">
+            <img src="@/assets/img/mymail.png" alt="">
+            <p class="card_p">web_wuxian@163.com</p>
+          </div>
         </div>
       </div>
     </div>
     <div class="center">
+      <!-- about me -->
       <div class="center_top">
         <div class="card">
-          <p class="title" @click="att()">Photo</p>
+          <p class="title" @click="att()">About Me</p>
         </div>
       </div>
       <div class="center_bottom">
+        <!-- github -->
         <div class="center_bottom_box" style="margin-right: 10px">
-          <div class="card">
-            <p class="title" @click="att()">Photo</p>
+          <div class="card git_box">
+            <p class="title" @click="att()">GitHub</p>
+            <div class="photo_box">
+              <img src="@/assets/img/mygit.png" alt="">
+              <a class="card_p" href="https://github.com/Xu-Jie">https://github.com/Xu-Jie</a>
+            </div>
           </div>
         </div>
+        <!-- wechart -->
         <div class="center_bottom_box">
-          <div class="card">
-            <p class="title" @click="att()">Photo</p>
+          <div class="card chart_box">
+            <p class="title" @click="att()">WeChart</p>
+            <div class="photo_box">
+              <img class="chart_img" src="@/assets/img/mychart.jpg" alt="">
+              <p class="card_p">jj1776338251</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div class="right">
+      <!-- where -->
       <div class="right_top">
-        <div class="card">
-          <p class="title">Photo</p>
+        <div class="card where_box">
+          <p class="title">Where</p>
+          <div class="photo_box">
+            <img src="@/assets/img/where.png" alt="">
+          </div>
         </div>
       </div>
+      <!-- friends -->
       <div class="right_bottom">
-        <div class="card">
-          <p class="title" @click="att()">Photo</p>
+        <div class="card friends_box">
+          <p class="title" @click="att()">Friends</p>
+          <ul class="friends_content">
+            <li>
+              <div class="li_title">三宝</div>
+              <a href="https://github.com/Javison666">前端大佬</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -123,5 +154,69 @@ export default class Home extends Vue {
 }
 .right_bottom {
   height: 64%;
+}
+.photo_card {
+  background-color: #ffd03f;
+}
+.photo_box {
+  width: 100%;
+  height: calc(100% - 40px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.photo_img {
+  width: 60%;
+}
+.mail_box {
+  background-color: #ff98c6;
+}
+.card_p {
+  margin-top: 10px;
+  color: #000;
+}
+.git_box {
+  background-color: #817ef8;
+}
+.chart_box {
+  background-color: #2fd2db;
+}
+.where_box {
+  background-color: #2fd2db;
+}
+.friends_box {
+  background-color: #ff98c6;
+}
+.chart_img {
+  width: 122px;
+}
+.friends_content {
+  padding: 30px;
+  width: 100%;
+  height: calc(100% -40px);
+  box-sizing: border-box;
+}
+.friends_content li {
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 40px;
+  line-height: 40px;
+}
+.li_title {
+  width: 100px;
+  color: #000;
+  font-size: 18px;
+  font-weight: normal;
+}
+.friends_content li a {
+  font-size: 14px;
+  color: #aa6584;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.friends_content li a:hover {
+  text-decoration: underline;
 }
 </style>
